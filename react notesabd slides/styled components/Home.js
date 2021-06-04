50ed1b94378253d2fe210fca75cafbb915bb9db9
wa5 <47422872+wa5@users.noreply.github.com>
2021-06-04 06:12:21 -0700

    Add files via upload

---
+++ react notesabd slides/styled components/Home.js
@@ -0,0 +1,17 @@
+import Navbarss from "../components/Navbarss/Navbarss"
+import { Button, Button2 } from "../components/styled-components/Button.style"
+
+
+
+function Home() {
+    return (<>
+    <Button>clic me</Button>
+        <Button2 backgroundcolor="yellow" >click me</Button2>
+        <Button2 backgroundcolor="green" >click me</Button2>
+<Navbarss/>
+
+</>
+    )
+}
+
+export default Home
