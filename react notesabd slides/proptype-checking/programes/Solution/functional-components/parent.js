971d7c3afee2fd79b3c1992a0d4c8a0d2e6ea1c8
wa5 <47422872+wa5@users.noreply.github.com>
2021-06-11 07:41:46 +0530

    Create parent.js

---
+++ react notesabd slides/proptype-checking/programes/Solution/functional-components/parent.js
@@ -0,0 +1,24 @@
+import React,{Component}from 'react'
+import Child from './Child'
+import H from './H'
+
+
+export class Parent extends Component {
+    
+    render() {
+        return (
+            <div>
+                <h1>im a parent component</h1>
+                <h3>im a child <Child name={"salman"} age={"hero"}/></h3>
+                {/* problem age should conatin integer but it shows string it accpts it */}
+                <H name={"salman"} age={"hero"}/>
+            </div>
+        )
+    }
+
+
+    
+}
+
+export default Parent
+
