f2a5d0d48e9c82ed1ce926e1905ceaa6b351c1b5
wa5 <47422872+wa5@users.noreply.github.com>
2021-06-12 07:21:08 +0530

    Create class-props.js

---
+++ react notesabd slides/proptype-checking/programes/defaulttype-program/class-props.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+ class H extends Component {
+    static propTypes = {
+        age:PropTypes.number
+    }
+    static defaultProps = {
+        name: "secondary",
+        
+      }
+
+    render() {
+        return (
+            <div>
+                
+                <h1>{this.props.name}</h1>
+                <h2>{this.props.age}</h2>
+                
+            </div>
+        )
+    }
+
+   
+}
+
+export default H
