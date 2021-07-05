d1de09e2aef632d8b91485ed39853b69e95e20dc
wa5 <47422872+wa5@users.noreply.github.com>
2021-07-05 19:30:35 +0530

    Create calling.js

---
+++ react notesabd slides/styled components/button/calling.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button, GreenButton } from '../components.style/Button'
+
+function Stcom() {
+    return (
+        <div>
+            <p>hello all i  a styled component library
+
+            </p>
+        <Button>click me</Button>
+        <GreenButton  backgroundcolor="red">clicke</GreenButton>
+        <GreenButton  backgroundcolor="blue">clicke</GreenButton>
+        <GreenButton  backgroundcolor="black">clicke</GreenButton>
+        <GreenButton  backgroundcolor="red">clicke</GreenButton>
+        </div>
+    )
+}
+
+export default Stcom
